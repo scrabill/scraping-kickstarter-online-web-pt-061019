b26a621da41c62b2095e95be10c7a07f84df7b25
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-26 19:36:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,18 @@
 # require libraries/modules here
 
+require "nokogiri"
+require "pry"
+
 def create_project_hash
   # write your code here
+
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
+  kickstarter.css("li.project.grid_4").first
+  kickstarter.css("li.project.grid_4 h2.bbcard_name")
+  kickstarter.css("li.project.grid_4 h2.bbcard_name").first.text.strip
+  kickstarter.css("li.project.grid_4 p.bbcard_blurb").first.text.strip
+  kickstarter.css("li.project.grid_4 img.projectphoto-little").
+  binding.pry
 end
