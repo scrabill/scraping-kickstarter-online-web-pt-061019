91771e0ed9074ed1d00eb3ba42558cdca5e1b80e
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-26 20:00:22 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -28,12 +28,14 @@ def create_project_hash
 
   projects
 
-  # kickstarter.css("li.project.grid_4").first
-  # kickstarter.css("li.project.grid_4 h2.bbcard_name")
-  # kickstarter.css("li.project.grid_4 h2.bbcard_name").first.text.strip
-  # kickstarter.css("li.project.grid_4 p.bbcard_blurb").first.text.strip
-  # kickstarter.css("li.project.grid_4 img.projectphoto-little").attribute("src").value
-  # kickstarter.css("li.project.grid_4 span.location-name").first.text.strip
-  # kickstarter.css("li.project.grid_4 li.first strong").first.text.strip
-  # binding.pry
+
 end
+
+# kickstarter.css("li.project.grid_4").first
+# kickstarter.css("li.project.grid_4 h2.bbcard_name")
+# kickstarter.css("li.project.grid_4 h2.bbcard_name").first.text.strip
+# kickstarter.css("li.project.grid_4 p.bbcard_blurb").first.text.strip
+# kickstarter.css("li.project.grid_4 img.projectphoto-little").attribute("src").value
+# kickstarter.css("li.project.grid_4 span.location-name").first.text.strip
+# kickstarter.css("li.project.grid_4 li.first strong").first.text.strip
+# binding.pry
