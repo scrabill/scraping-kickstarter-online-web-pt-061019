2840780cfc67c685a063a0befa4187f06a8438be
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-26 19:40:54 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,7 +14,6 @@ def create_project_hash
   kickstarter.css("li.project.grid_4 h2.bbcard_name").first.text.strip
   kickstarter.css("li.project.grid_4 p.bbcard_blurb").first.text.strip
   kickstarter.css("li.project.grid_4 img.projectphoto-little").attribute("src").value
-  kickstarter.css("li.project.grid_4 span.location-name").attribute("src").value
-  location-name
+  kickstarter.css("li.project.grid_4 span.location-name")
   binding.pry
 end
