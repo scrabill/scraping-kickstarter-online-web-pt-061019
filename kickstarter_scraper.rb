af201bad68f659f47f7a5585d6bb070f30fc9ceb
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-26 19:47:48 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,7 +13,7 @@ def create_project_hash
 
   kickstarter.css("li.project.grid_4").each do |project|
     title = kickstarter.css("li.project.grid_4 h2.bbcard_name").text
-    projects[title.to_sym] = []
+    projects[title.to_sym] = {}
   end
 
   projects
