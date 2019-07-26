8015fd27a305c5d9bf801ccebf66845cd6b6b87c
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-26 19:45:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,12 +9,12 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-  kickstarter.css("li.project.grid_4").first
-  kickstarter.css("li.project.grid_4 h2.bbcard_name")
-  kickstarter.css("li.project.grid_4 h2.bbcard_name").first.text.strip
-  kickstarter.css("li.project.grid_4 p.bbcard_blurb").first.text.strip
-  kickstarter.css("li.project.grid_4 img.projectphoto-little").attribute("src").value
-  kickstarter.css("li.project.grid_4 span.location-name").first.text.strip
-  kickstarter.css("li.project.grid_4 li.first strong").first.text.strip
-  binding.pry
+  # kickstarter.css("li.project.grid_4").first
+  # kickstarter.css("li.project.grid_4 h2.bbcard_name")
+  # kickstarter.css("li.project.grid_4 h2.bbcard_name").first.text.strip
+  # kickstarter.css("li.project.grid_4 p.bbcard_blurb").first.text.strip
+  # kickstarter.css("li.project.grid_4 img.projectphoto-little").attribute("src").value
+  # kickstarter.css("li.project.grid_4 span.location-name").first.text.strip
+  # kickstarter.css("li.project.grid_4 li.first strong").first.text.strip
+  # binding.pry
 end
