122f6d819b22979290bed4eae084ff8523541689
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-26 20:09:31 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,10 +14,10 @@ def create_project_hash
   kickstarter.css("li.project.grid_4").each do |project|
     title = kickstarter.css("li.project.grid_4 h2.bbcard_name").text
     projects[title.to_sym] = {
-      :image_link => kickstarter.css("li.project.grid_4 img.projectphoto-little").attribute("src").value,
-      :description => kickstarter.css("li.project.grid_4 p.bbcard_blurb").text.strip,
-      :location => kickstarter.css("li.project.grid_4 span.location-name").text.strip,
-      :percent_funded => kickstarter.css("li.project.grid_4 li.first strong").text.gsub("%","").to_i
+      :image_link => project.css("img.projectphoto-little").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text.strip,
+      :location => project.css("span.location-name").text.strip,
+      :percent_funded => project.css("i.first strong").text.gsub("%","").to_i
     }
   end
   # binding.pry
